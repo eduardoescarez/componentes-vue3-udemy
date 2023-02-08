569ddf91a9cd7fb82552f82ea31b10a1d2705305
Eduardo Escárez <eduardoe@gmail.com>
2023-02-08 18:21:03 -0300

    Sección 45 y 46 - instala bootstrap y props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
     import ButtonCounter from "./components/ButtonCounter.vue"
+    import BlogPost from "./components/BlogPost.vue"
 </script>
 
 
 <template>
     <div class="container">
         <h1>APP</h1>
-        <ButtonCounter> </ButtonCounter>
-        <button-counter> </button-counter>
+        <ButtonCounter/>
+        <BlogPost title="Post 1" />
+        <BlogPost title="Post 2" />
+        <BlogPost title="Post 3" />
+        <BlogPost title="Post 4" />
     </div>
 </template>
